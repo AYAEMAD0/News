00b69481cf692e8d3a9360cf3e8efadc5dc826b8
Aya Emad <ayaelbaroudy0@gmail.com>
2025-10-22 19:00:50 +0300

    + add arch for project

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+class AppTheme {
+  static final ThemeData themeLight = ThemeData(
+    scaffoldBackgroundColor: AppColors.primaryLight,
+  );
+  static final ThemeData themeDark = ThemeData(
+    scaffoldBackgroundColor: AppColors.primaryDark,
+  );
+}
