f4b534c2b11569ef3ee71f41a073648d6ea7962e
Aya Emad <ayaelbaroudy0@gmail.com>
2025-10-24 20:08:37 +0300

    + cash theme using shared preference

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:news/core/helper/shared_check_helper.dart';
 import 'package:news/core/theme/app_theme.dart';
 import 'package:news/features/views/home/home_view.dart';
 import 'package:news/provider/theme_provider/theme_provider.dart';
 import 'package:provider/provider.dart';
 import 'core/routing/routes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SharedCheckHelper.init();
+  final bool isDark = await SharedCheckHelper.getTheme();
   runApp(
     ChangeNotifierProvider(
-      create: (context) => ThemeProvider(),
+      create: (context) => ThemeProvider(isDark),
       child: const MyApp(),
     ),
   );
@@ -18,12 +22,13 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    var theme=Provider.of<ThemeProvider>(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {Routes.homeRouteName: (context) => HomeView()},
       theme: AppTheme.themeLight,
       darkTheme: AppTheme.themeDark,
-      themeMode: Provider.of<ThemeProvider>(context).themeApp,
+      themeMode:theme.themeApp ,
       initialRoute: Routes.homeRouteName,
     );
   }
--- lib/provider/theme_provider/theme_provider.dart
+++ lib/provider/theme_provider/theme_provider.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:news/core/helper/shared_check_helper.dart';
+
 class ThemeProvider extends ChangeNotifier {
-  late ThemeMode themeApp = ThemeMode.dark;
-  Future<void> changeTheme(newTheme) async {
+  late ThemeMode themeApp;
+
+  ThemeProvider(bool isDark) {
+    themeApp = isDark ? ThemeMode.dark : ThemeMode.light;
+  }
+  Future<void> changeTheme(ThemeMode newTheme) async {
     if (newTheme == themeApp) {
       return;
     } else {
       themeApp = newTheme;
     }
+    await SharedCheckHelper.setTheme(isDark());
     notifyListeners();
   }
-
   bool isDark() {
     return themeApp == ThemeMode.dark;
   }
