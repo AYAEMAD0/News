08457eff691a4c2235be25520c98c2e91c9eac50
Aya Emad <ayaelbaroudy0@gmail.com>
2025-10-24 13:37:36 +0300

    + add packge get_time_ago to convert time

--- lib/features/views/home/category_details/news/news_item.dart
+++ lib/features/views/home/category_details/news/news_item.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get_time_ago/get_time_ago.dart';
 import 'package:news/core/constants/app_strings.dart';
 import 'package:news/core/theme/app_colors.dart';
 import 'package:news/model/news_response.dart';
@@ -13,6 +14,10 @@ class NewsItem extends StatelessWidget {
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
+
+    var convertedTimestamp = DateTime.parse(news.publishedAt??"");
+    var time = GetTimeAgo.parse(convertedTimestamp);
+
     return Container(
       padding: EdgeInsets.symmetric(
         horizontal: width * 0.020,
@@ -33,7 +38,7 @@ class NewsItem extends StatelessWidget {
             child: CachedNetworkImage(
               imageUrl: news.urlToImage??"",
               placeholder: (context, url) => CircularProgressIndicator(color:Theme.of(context).canvasColor ),
-              errorWidget: (context, url, error) => Icon(Icons.error,color: AppColors.gray,),
+              errorWidget: (context, url, error) => Icon(Icons.error,color: AppColors.gray,size: 40,),
             ),
           ),
           Text(news.title??AppStrings.noTitle, style: Theme.of(context).textTheme.labelLarge),
@@ -41,8 +46,8 @@ class NewsItem extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Expanded(child: Text("By: ${news.author}", style: TextStyles.font14GrayMedium)),
-              //todo convert time min
-              Text(news.publishedAt??"", style: TextStyles.font14GrayMedium),
+              //todo convert time minute  2025-10-23T09:42:28Z
+              Text(time, style: TextStyles.font14GrayMedium),
             ],
           ),
         ],
